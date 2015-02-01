28d64a6b6de6da4d6ea55975937fc6fbb501a593
KimmoCommit <registerstuffhere@gmail.com>
2015-02-01 16:49:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 $(function(){
     $('body').append('<h1>Hola, {{yourName}}</h1>');
+    
+    $('body').append('<div id="main-container" ng-controller="searchController"><div class="container container-centered"><input type="text" placeholder="search" id="search" ng-model="post.filter"> <li ng-repeat="post in Posts | filter: post.filter">{{post.Title}}</li></div></div>');
   });
