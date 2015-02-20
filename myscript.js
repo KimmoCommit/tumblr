5b5162f2c9995c6d6e5f38414fbcb0a2b053e2d3
KimmoCommit <registerstuffhere@gmail.com>
2015-02-20 14:58:35 +0200

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,5 @@
+    $(function(){
+        $('body').load("https://raw.githubusercontent.com/KimmoCommit/tumblr/master/testing.html", function(res,stat,xhr){
+        console.log("done!!!"); 
+        });    
+    });
